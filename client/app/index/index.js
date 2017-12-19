260e1288d79d7846fdd12dde5573d6b842ad13e2
王俊 <wangjun@dinglicom.com>
2017-12-19 20:13:00 +0800

    'addpage'

--- client/app/index/index.js
+++ client/app/index/index.js
@@ -13,5 +13,44 @@ angular.module('FlyWebApp')
                     }
                 }
             })
-
+            .state('about', {
+                url   : '/about',
+                access: {requiredLogin: false},
+                views : {
+                    'root': {
+                        templateUrl: 'app/index/about.html',
+                        controller : 'IndexCtrl'
+                    }
+                }
+            })
+            .state('article', {
+                url   : '/article',
+                access: {requiredLogin: false},
+                views : {
+                    'root': {
+                        templateUrl: 'app/index/article.html',
+                        controller : 'IndexCtrl'
+                    }
+                }
+            })
+            .state('our', {
+                url   : '/our',
+                access: {requiredLogin: false},
+                views : {
+                    'root': {
+                        templateUrl: 'app/index/our.html',
+                        controller : 'IndexCtrl'
+                    }
+                }
+            })
+            .state('contact', {
+                url   : '/contact',
+                access: {requiredLogin: false},
+                views : {
+                    'root': {
+                        templateUrl: 'app/index/our.html',
+                        controller : 'IndexCtrl'
+                    }
+                }
+            })
     });
